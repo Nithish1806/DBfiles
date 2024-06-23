d13d97873c9e29c0c5ad78da8c097b4065f7524e
Vibiksha <98578149+vibiksha@users.noreply.github.com>
2024-06-23 19:40:21 +0530

    Add files via upload

--- routes/history.js
+++ routes/history.js
@@ -1,16 +1,17 @@
-// routes/history.js
 const express = require('express');
 const router = express.Router();
-const { getTiingoData } = require('../services/fetchService');
+const { fetchDataFromMongo, collectionNames } = require('../services/fetchService');
 
 router.get('/', async (req, res) => {
     try {
-        const { keyword, startDate } = req.query;
-        const date = new Date(startDate);
-        date.setFullYear(date.getFullYear() - 2);
-        const formattedDate = date.toISOString().split('T')[0];
-        const urlCharts = 'https://api.tiingo.com/tiingo/daily/'+keyword+'/prices?startDate='+formattedDate+'&token=dba09041d3f74d6661a2e620087813c35682f331'
-        const data = await getTiingoData(urlCharts);
+        const keyword = req.query.keyword;
+        const query = {
+            'ticker': keyword
+        };
+
+        // Fetch data from MongoDB
+        const data = await fetchDataFromMongo(collectionNames.history, query);
+
         res.json(data);
     } catch (error) {
         res.status(500).send('Error fetching historical data');
